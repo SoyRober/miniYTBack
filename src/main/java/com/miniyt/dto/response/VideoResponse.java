3b72e7a869de924b2639660ddf9a3fb197955bf9
SoyRober <roberlamilagrosa@gmail.com>
2025-06-30 13:02:37 +0200

    Using lombok

--- src/main/java/com/miniyt/dto/response/VideoResponse.java
+++ src/main/java/com/miniyt/dto/response/VideoResponse.java
@@ -1,6 +1,17 @@
 package com.miniyt.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VideoResponse {
+    private Long id;
+
     private String uuid;
 
     private String title;
@@ -8,43 +19,4 @@ public class VideoResponse {
     private String description;
 
     private String thumbnail;
-
-    public VideoResponse(String uuid, String title, String description, String thumbnail) {
-        this.uuid = uuid;
-        this.title = title;
-        this.description = description;
-        this.thumbnail = thumbnail;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getThumbnail() {
-        return thumbnail;
-    }
-
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
 }
